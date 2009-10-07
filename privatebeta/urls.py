0b913277ce636315a989e339cd4544c6206856a3
Matt Croydon <matt@lawrence.com>
2009-10-07 00:16:58 -0500

    Fixed typo in urlconf.

--- privatebeta/urls.py
+++ privatebeta/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'privatebeta.views.invite', name='privatebeta_invite'),
-    url(r'^sent/$', 'privatebeta.views.sent', name='privatebeta_invite'),
+    url(r'^sent/$', 'privatebeta.views.sent', name='privatebeta_sent'),
 )
