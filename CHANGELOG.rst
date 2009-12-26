ed90691da7bdd0d9b5a9feab249fdde695254d3f
Matt Croydon <matt@lawrence.com>
2009-12-25 21:37:07 -0600

    Added MANIFEST.in, updated setup.py to include migrations and rst files in source distribution.  Version 0.4.1.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,12 @@
 Changelog
 =========
 
+Version 0.4.1
+=============
+
+* Added MANIFEST.in, updated setup.py to include migrations and rst files in
+  source distribution.
+
 Version 0.4.0
 =============
 
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='privatebeta',
-      version='0.4.0',
+      version='0.4.1',
       description='A reusable application for collecting email addresses for later invitations and to restrict access to a site under private beta.',
       author='Pragmatic Badger, LLC.',
       author_email='info@pragmaticbadger.com',
       url='http://github.com/pragmaticbadger/django-privatebeta/',
-      packages=['privatebeta'],
+      packages=['privatebeta', 'privatebeta.migrations'],
       package_dir={'privatebeta': 'privatebeta'},
      )
